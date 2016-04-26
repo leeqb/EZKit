0c396d8c62febc051523c6af089b7ea2b9340d38
xboy <dolphin2525@163.com>
2016-04-26 16:34:40 +0800

    初始提交

---
+++ EZKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EZKit
+//
+//  Created by leeqb on 4/26/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EZKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZKit
+//
+//  Created by leeqb on 4/26/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
