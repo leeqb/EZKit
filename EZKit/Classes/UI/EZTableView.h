f8779931a559063a8caf33c65af4a63cd9ab9f19
xboy <dolphin2525@163.com>
2016-04-27 15:07:44 +0800

    ...

---
+++ EZKit/Classes/UI/EZTableView.h
@@ -0,0 +1,16 @@
+//
+//  EZTableView.h
+//  EZKit
+//
+//  Created by leeqb on 4/26/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EZTableView : UITableView
+
+@property (nonatomic, copy) NSString *url;
+@property (nonatomic, copy) NSDictionary *params;
+
+@end
---
+++ EZKit/Classes/UI/EZTableView.m
@@ -0,0 +1,27 @@
+//
+//  EZTableView.m
+//  EZKit
+//
+//  Created by leeqb on 4/26/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#import "EZTableView.h"
+
+@implementation EZTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (void)reloadData
+{
+    [super reloadData];
+    
+}
+
+@end
