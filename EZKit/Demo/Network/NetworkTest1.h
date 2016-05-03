0ae5d8fb614e94d757cc7f74a8c34e0a0a7a0538
xboy <dolphin2525@163.com>
2016-05-03 14:05:22 +0800

    ...

---
+++ EZKit/Demo/Network/NetworkTest1.h
@@ -0,0 +1,13 @@
+//
+//  NetworkTest1.h
+//  EZKit
+//
+//  Created by leeqb on 5/3/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkTest1 : NSObject
+
+@end
---
+++ EZKit/Demo/Network/NetworkTest1.m
@@ -0,0 +1,13 @@
+//
+//  NetworkTest1.m
+//  EZKit
+//
+//  Created by leeqb on 5/3/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#import "NetworkTest1.h"
+
+@implementation NetworkTest1
+
+@end
