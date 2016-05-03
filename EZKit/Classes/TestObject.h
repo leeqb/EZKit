96faa92910f9f8759833ad554eb210becc4bffba
xboy <dolphin2525@163.com>
2016-05-03 18:09:41 +0800

    ...

---
+++ EZKit/Classes/TestObject.h
@@ -0,0 +1,13 @@
+//
+//  TestObject.h
+//  EZKit
+//
+//  Created by leeqb on 5/3/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+
+@end
---
+++ EZKit/Classes/TestObject.m
@@ -0,0 +1,13 @@
+//
+//  TestObject.m
+//  EZKit
+//
+//  Created by leeqb on 5/3/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
