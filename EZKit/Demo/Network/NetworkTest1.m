541a640c8af24cc99c2fdd8fdc0529b0040c08ac
xboy <dolphin2525@163.com>
2016-05-03 18:53:08 +0800

    ...

--- EZKit/Demo/Network/NetworkTest1.m
+++ EZKit/Demo/Network/NetworkTest1.m
@@ -13,6 +13,7 @@
 - (void)startRequest
 {
     [[EZNetwork sharedInstance] POST:@"http://api.map.baidu.com/telematics/v3/weather?location=%E5%98%89%E5%85%B4&output=json&ak=5slgyqGDENN7Sy7pw29IUvrZ" params:nil success:^(NSDictionary *result) {
+        NSLog(@"%@", result);
     } failed:^(NSDictionary *result, NSError *error) {
     } finally:^{
     }];
