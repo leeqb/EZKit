c59114d10aec2691c84e2e6cddee56a57e87c1d7
xboy <dolphin2525@163.com>
2016-05-13 03:49:26 +0800

    新增EZKit.h头文件

---
+++ EZKit/Classes/EZKit.h
@@ -0,0 +1,14 @@
+//
+//  EZKit.h
+//  EZKit
+//
+//  Created by leeqb on 5/13/16.
+//  Copyright © 2016 powerlee. All rights reserved.
+//
+
+#ifndef EZKit_h
+#define EZKit_h
+
+#import "EZNetwork.h"
+
+#endif /* EZKit_h */
