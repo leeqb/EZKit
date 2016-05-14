db6a92a6469785184f0c2f3262a676feb26c8b2a
xboy <dolphin2525@163.com>
2016-05-14 09:36:05 +0800

    ...

---
+++ EZKit/Classes/Category/UITableView+Extension.h
@@ -0,0 +1,15 @@
+//
+//  UITableView+Extension.h
+//  CodeBase
+//
+//  Created by leeqb on 11/8/15.
+//  Copyright © 2015 powerlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (Extension)
+
+- (void)zeroInsets;
+
+@end
---
+++ EZKit/Classes/Category/UITableViewCell+Extension.h
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+Extension.h
+//  CodeBase
+//
+//  Created by leeqb on 11/8/15.
+//  Copyright © 2015 powerlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewCell (Extension)
+
+- (void)zeroInsets;
+
+@end
