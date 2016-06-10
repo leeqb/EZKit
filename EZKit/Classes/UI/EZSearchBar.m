bfcae11ef4855e1a0c37368174a6863db8795cec
xboy <dolphin2525@163.com>
2016-06-10 17:15:55 +0800

    搜索框实现

--- EZKit/Classes/UI/EZSearchBar.m
+++ EZKit/Classes/UI/EZSearchBar.m
@@ -9,6 +9,8 @@
 #import "EZSearchBar.h"
 
 @interface EZSearchBar()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *inputWidth;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
@@ -49,16 +51,45 @@
     rect.origin.y = 0;
     
     NSBundle *bundle = [NSBundle bundleForClass:self.class];
-    UINib *nib = [UINib nibWithNibName:@"EZSearchBar" bundle:bundle];
+    UINib *nib = [UINib nibWithNibName:NSStringFromClass(self.class) bundle:bundle];
     UIView *contentView = [[nib instantiateWithOwner:self options:nil] firstObject];
     contentView.frame = rect;
     [self addSubview:contentView];
+    
+    // 隔0.01秒再执行，否则宽度还未自适应
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.01 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        _inputWidth.constant = self.frame.size.width / 2;
+    });
+}
+
+#pragma mark - Actions
+- (IBAction)selfTapped:(id)sender {
+    [_textField becomeFirstResponder];
+    [UIView animateWithDuration:0.3 animations:^{
+        _inputWidth.constant = self.frame.size.width - 36;
+        [self layoutIfNeeded];
+    }];
 }
 
 #pragma mark - UITextFieldDelegate
 - (void)textFieldDidBeginEditing:(UITextField *)textField
 {
+    [_textField becomeFirstResponder];
+    [UIView animateWithDuration:0.3 animations:^{
+        _inputWidth.constant = self.frame.size.width - 36;
+        [self layoutIfNeeded];
+    }];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    [UIView animateWithDuration:0.3 animations:^{
+        _inputWidth.constant = self.frame.size.width / 2;
+        [self layoutIfNeeded];
+    }];
     
+    return YES;
 }
 
 @end
